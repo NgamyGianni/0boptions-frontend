b9cd26dd76b461f501d599d3860f96322d8d1cc6
NgamyGianni <spyker97160@hotmail.fr>
2021-11-12 18:40:10 +0100

    ajout address et state

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,16 +1,64 @@
-import React from "react"
+import React, { Component } from "react"
 import styled from "styled-components"
 import { FaEthereum } from "react-icons/fa"
+import Web3 from 'web3'
 
-const Portfolio = () => {
-	return (
-		<Container>
-			<Currency>
-				<FaEthereum />
-			</Currency>
-			<Money>0 ETH</Money>
-		</Container>
-	)
+export let selectedAccount;
+
+class Portfolio extends Component{
+
+	constructor(){
+		super();
+		this.state = {
+			address: '',
+			balance: '',
+			network: ''
+		}
+	}
+
+	componentWillMount(){
+		this.logAccount();
+	}
+
+	async logAccount(){
+		let provider = window.ethereum;
+		const web3 = new Web3(window.ethereum);
+		if (typeof provider !== 'undefined') {
+			await provider
+				.request({ method: 'eth_requestAccounts' })
+				.then((accounts) => {
+					selectedAccount = accounts[0];
+					console.log(`Selected account is ${selectedAccount}`);
+				})
+				.catch((err) => {
+					console.log(err);
+					return;
+				});
+
+			await window.ethereum.on('accountsChanged', function (accounts) {
+				selectedAccount = accounts[0];
+				console.log(`Selected account changed to ${selectedAccount}`);
+			});
+			this.setState({ 
+		    	account: selectedAccount,
+		    	balance: web3.utils.fromWei(await web3.eth.getBalance(selectedAccount), 'ether'),
+    			network: await web3.eth.net.getNetworkType()
+		    });
+		}
+	}
+
+	render() {
+		return (
+			<Container>
+				<Currency>
+					<FaEthereum />
+					Ethereum
+				</Currency>
+				<Money>0 ETH</Money>
+				<Address>{this.state.account}</Address>
+			</Container>
+		)
+	}
 }
 
 const Container = styled.div`
@@ -18,7 +66,7 @@ const Container = styled.div`
 	padding: 0.5rem;
 	border-radius: 0.5rem;
 	display: flex;
-	width: 150px;
+	width: 40%;
 	justify-content: space-between;
 `
 
@@ -30,6 +78,13 @@ const Currency = styled.div`
 `
 
 const Money = styled.div`
+	background-color: #2f3031;
+	color: white;
+	padding: 0.5rem;
+	border-radius: 0.5rem;
+`
+
+const Address = styled.div`
 	background-color: #2f3031;
 	color: white;
 	padding: 0.5rem;
--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,24 +1,79 @@
-import React from "react"
+import React, { Component } from "react"
 import styled from "styled-components"
 import Betting from "../Components/Betting"
 import { BiFootball, BiBasketball, BiTennisBall } from "react-icons/bi"
 import { MdSportsRugby } from "react-icons/md"
 import Navbar from "../Components/Navbar"
-const Homepage = () => {
-	return (
-		<Main>
-			<Navbar />
-			<Container>
-				<Categories>
-					<BiFootball className="active" />
-					<BiBasketball />
-					<BiTennisBall />
-					<MdSportsRugby />
-				</Categories>
-				<Betting />
-			</Container>
-		</Main>
-	)
+import Portfolio from "../Components/Portfolio"
+import Web3 from 'web3'
+
+export let selectedAccount;
+
+class Homepage extends Component{
+
+	componentWillMount(){
+		this.loadAccount();
+	}
+
+	async loadAccount(){
+		let provider = window.ethereum;
+		const web3 = new Web3(window.ethereum);
+		if (typeof provider !== 'undefined') {
+			await provider
+				.request({ method: 'eth_requestAccounts' })
+				.then((accounts) => {
+					selectedAccount = accounts[0];
+					console.log(`Selected account is ${selectedAccount}`);
+				})
+				.catch((err) => {
+					console.log(err);
+					return;
+				});
+
+			await window.ethereum.on('accountsChanged', function (accounts) {
+				selectedAccount = accounts[0];
+				console.log(`Selected account changed to ${selectedAccount}`);
+			});
+			await this.setState({ 
+		    	account: selectedAccount, 
+		    });
+		}
+    	this.setState({ 
+    		balance: web3.utils.fromWei(await web3.eth.getBalance(selectedAccount), 'ether'),
+    		network: await web3.eth.net.getNetworkType()
+    	});
+    	console.log(this.state.account)
+    	console.log(this.state.balance)
+    	console.log(this.state.network)
+    	console.log(this.state.contract)
+	}
+
+	constructor(){
+		super();
+		this.state = {
+			account: '',
+			contract: '0xc4Cb5b35ED0a23DBbF6D9e7Dd46278D13Ce12264',
+			balance: '',
+			network: ''
+		}
+	}
+
+	render(){
+		return (
+			<Main>
+				<Navbar />
+				<Container>
+					<Categories>
+						<BiFootball className="active" />
+						<BiBasketball />
+						<BiTennisBall />
+						<MdSportsRugby />
+					</Categories>
+					<Betting />
+				</Container>
+			</Main>
+		)
+	}
 }
 
 const Main = styled.main`
